2906051ac6d67a8a33a441b5d188f0f19760a060
Brian "Beej Jorgensen" Hall <beej@beej.us>
2019-01-13 13:53:44 -0800

    add code for markup, change popup position

--- src/win.rs
+++ src/win.rs
@@ -1,13 +1,12 @@
 /// Windows-related functions
-
 use crate::G;
 use ncurses::*;
 
 impl G {
     /// Set or clear output attributes on a window.
-    /// 
+    ///
     /// See `new()` for a list of attributes.
-    /// 
+    ///
     /// You probably want `wcon()` or `wcoff()` instead.
     pub fn wcset(&self, w: WINDOW, c: &str, on: bool) {
         if let Some(attr) = self.color.get(c) {
@@ -33,32 +32,81 @@ impl G {
 
     /// Print some text in the center of a window, ignoring leading and trailing
     /// whitespace.
-    pub fn wprintw_center(w: WINDOW, s: &str) -> i32{
+    pub fn wprintw_center(w: WINDOW, s: &str) {
         G::mvwprintw_center(w, getcury(w), s)
     }
 
     /// Print some text in the center of a window, ignoring leading and trailing
     /// whitespace, given a `y` position.
-    pub fn mvwprintw_center(w: WINDOW, y: i32, s: &str) -> i32 {
-        let x = (getmaxx(w) - s.trim().len() as i32) / 2;
+    ///
+    /// If the string contains pipes like so:
+    ///
+    ///    "|Foo| bar baz |E|tc"
+    ///
+    /// The contents between the pipes will be shown in A_REVERSE.
+    pub fn mvwprintw_center(w: WINDOW, y: i32, s: &str) {
+        let ts = s.trim();
+        let mut len = 0;
+        let mut has_markup = false;
 
-        mvwprintw(w, y, x, s)
+        for c in ts.chars() {
+            if c != '|' {
+                len += 1;
+            } else {
+                has_markup = true;
+            }
+        }
+
+        let x = (getmaxx(w) - len) / 2;
+
+        wmove(w, y, x);
+
+        let mut reversed = false;
+
+        if has_markup {
+            wattr_off(w, A_REVERSE());
+        }
+
+        for c in s.chars() {
+            if c == '|' {
+                reversed = !reversed;
+
+                if reversed {
+                    wattr_on(w, A_REVERSE());
+                } else {
+                    wattr_off(w, A_REVERSE());
+                }
+                continue;
+            }
+
+            wprintw(w, &format!("{}", c));
+        }
     }
 
     /// Pop-up a new window of a given size with a border.
-    pub fn newwin_popup(lines: i32, cols: i32) -> WINDOW {
-        
-        let mut y = LINES() / 4 - lines / 2;
+    pub fn popup(lines: i32, cols: i32) -> WINDOW {
+        let mut y;
         let x = (COLS() - cols) / 2;
+
+        // Aesthetic positioning logic
+        if LINES() <= 25 && lines >= 12 {
+            y = (LINES() as f32 / 2.5) as i32 - lines / 2;
+        } else {
+            y = LINES() / 4 - lines / 2;
+        }
 
         if y < 0 {
             y = 0;
         }
 
-        let w = newwin(lines, cols, y, x);
-        box_(w, 0, 0);
+        newwin(lines, cols, y, x)
+    }
 
-        w
+    /// Close a popup
+    pub fn popup_close(w: WINDOW) {
+        wclear(w);
+        wrefresh(w);
+        delwin(w);
     }
 
     /// Show or hide the cursor.
@@ -69,4 +117,12 @@ impl G {
             curs_set(CURSOR_VISIBILITY::CURSOR_INVISIBLE);
         }
     }
+
+    /*
+    /// Clear the border
+    pub fn clear_border(w: WINDOW) {
+        let ch = ' ' as chtype;
+        wborder(w, ch, ch, ch, ch, ch, ch, ch, ch);
+    }
+    */
 }
