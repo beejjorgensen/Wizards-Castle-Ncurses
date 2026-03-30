c2b203cdd7f8e2eabedf8ecc2d8b1d211657b61e
Brian "Beej Jorgensen" Hall <beej@beej.us>
2026-03-30 13:43:05 -0700

    Fix mac crash on map

--- src/map.rs
+++ src/map.rs
@@ -7,12 +7,12 @@ impl G {
 
         werase(self.mapwin);
 
-        let _ = wprintw(self.mapwin, "\n ");
+        let _ = waddstr(self.mapwin, "\n ");
 
         for y in 0..self.game.dungeon_ysize() {
             for x in 0..self.game.dungeon_xsize() {
                 if x >= 1 {
-                    let _ = wprintw(self.mapwin, "   ");
+                    let _ = waddstr(self.mapwin, "   ");
                 }
 
                 let r = self.game.dungeon_room_at(x, y, z);
@@ -20,14 +20,14 @@ impl G {
                 let bracket = x == self.game.player_x() && y == self.game.player_y();
 
                 if self.game.player_is_blind() {
-                    let _ = wprintw(self.mapwin, " - ");
+                    let _ = waddstr(self.mapwin, " - ");
                 } else {
                     if bracket {
                         wattr_on(self.mapwin, A_BOLD);
-                        let _ = wprintw(self.mapwin, "<");
+                        let _ = waddstr(self.mapwin, "<");
                         wattr_off(self.mapwin, A_BOLD);
                     } else {
-                        let _ = wprintw(self.mapwin, " ");
+                        let _ = waddstr(self.mapwin, " ");
                     }
 
                     if r.discovered || show_all {
@@ -35,25 +35,25 @@ impl G {
                         let attr_str = format!("room-{}", room_ch);
 
                         self.wcon(self.mapwin, &attr_str);
-                        let _ = wprintw(self.mapwin, &format!("{}", room_ch));
+                        let _ = waddstr(self.mapwin, &format!("{}", room_ch));
                         self.wcoff(self.mapwin, &attr_str);
                     } else {
                         wattr_on(self.mapwin, A_DIM);
-                        let _ = wprintw(self.mapwin, "?");
+                        let _ = waddstr(self.mapwin, "?");
                         wattr_off(self.mapwin, A_DIM);
                     }
 
                     if bracket {
                         wattr_on(self.mapwin, A_BOLD);
-                        let _ = wprintw(self.mapwin, ">");
+                        let _ = waddstr(self.mapwin, ">");
                         wattr_off(self.mapwin, A_BOLD);
                     } else {
-                        let _ = wprintw(self.mapwin, " ");
+                        let _ = waddstr(self.mapwin, " ");
                     }
                 }
             }
 
-            let _ = wprintw(self.mapwin, "\n\n ");
+            let _ = waddstr(self.mapwin, "\n\n ");
         }
 
         box_(self.mapwin, 0, 0);
